a56559b26b8d7b110dc9af310c900fcc0b9d4a9d
subhi-png <subhi@apollo.lv>
2025-12-23 23:56:10 +0200

    Fix comment wording for Hero image description

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-black text-white">
-      {/* Hero с изображением Optimus */}
+      {/* Hero изображением Optimus */}
       <div className="relative w-full h-screen">
         <Image
           src="/optimus-hero.jpg"
